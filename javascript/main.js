f63a4cc10d35b5649d3267829987658b52f18dd9
engineeerahkhani <engineerahkhani@gmail.com>
2016-01-05 22:43:04 +0330

    this is my main project that i would develop

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(function(){
+	
+	//$('#my-name').fadeIn(7000);
+	
+//// and tell pending images to do the same, once they've finished loading:
+//$('#my-image').load(function () {
+//    $(this).slideToggle("5000");
+//	
+//	
+//	//$(this).show("slide", { direction: "left" }, 1000);
+//});
+//
+});
+ 
